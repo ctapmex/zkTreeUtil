a4c71a825446b8198b983efd4d590701d1bde6c0
Aleksey Dobrunov <ctapmex@gmail.com>
2014-10-24 20:01:52 +0600

    init commit

---
+++ src/main/java/com/dobrunov/zktreeutil/zkTreeUtilMain.java
@@ -0,0 +1,11 @@
+package com.dobrunov.zktreeutil;
+
+/**
+ * Created by ctapmex on 24.10.2014.
+ */
+public class zkTreeUtilMain {
+
+    public static void main(String[] args) {
+        System.out.println("start");
+    }
+}
